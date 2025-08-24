c53dccd82b4628ba74dc5789f6ec275c46916d6a
afkisch <afkisch@gmail.com>
2025-08-24 21:44:18 +0200

    Add homepage route

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from api.communication_api import comm_api
 from api.data_api import data_api
 from api.ingest_api import ingest_api
 from api.query_api import query_api
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.register_blueprint(comm_api)
 app.register_blueprint(data_api)
 app.register_blueprint(ingest_api)
@@ -12,7 +12,9 @@ app.register_blueprint(query_api)
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024  # 32 MB, for example
 
 
-
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
