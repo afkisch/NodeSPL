a9f2831617d41fc150f704783db3d7880a94543b
afkisch <afkisch@gmail.com>
2025-09-06 10:34:42 +0200

    Change output format and add saving to file

--- src/pipeline_runner_server.py
+++ src/pipeline_runner_server.py
@@ -1,35 +1,62 @@
 import yaml
 from dsp.processors import PROCESSING_FUNCTIONS
 
+import temp_state
+
 # TODO: Add error handling, just like in node-side processing
 # standardize processing function output format for a smooth proc. chain
 
-def run_server_pipelines(node_data, pipeline_yaml_path):
+# TODO: Fix file handling
+
+def run_server_pipelines(pipeline_yaml_path):
     """
     Runs all pipelines defined in YAML, returns dict of outputs.
     'type' is inferred dynamically.
     """
     with open(pipeline_yaml_path) as f:
         config = yaml.safe_load(f)
+    
+    node_id_list = config['nodes']
 
-    results = {}
-    for name, pipe in config["pipelines"].items():
-        data = node_data
-        for step_def in pipe.get("pipeline", []):
-            step_name = step_def["step"]
-            params = step_def.get("params", {})
-            processor = PROCESSING_FUNCTIONS[step_name]
-            data = processor(data, **params)
-        results[name] = {
-            "display_name": pipe.get("display_name", name),
-            "output_file": pipe.get("output_file", f"{name}.json"),
-            "node_data": data,
-            "type": type(data['value']).__name__  # inferred dynamically
+    result = {}
+    for node_id in node_id_list:
+        node_data = temp_state.latest_data.get(node_id, {'value': [0], 'last_seen': None})
+        result_node = list()
+        for name, pipe in config["pipelines"].items():
+            print("Pipeline ", name, " is being executed\n")
+            result_temp = None
+            for step_def in pipe.get("pipeline", []):
+                step_name = step_def["step"]
+                function = PROCESSING_FUNCTIONS.get(step_name)                      # Can be done 
+                if function:                                                        # using getattr()
+                    kwargs = step_def.get("params", {})                             # w/o dict
+                    kwargs['signal'] = node_data['value'] if result_temp is None else result_temp
+                    try:
+                        result_temp = function(**kwargs)
+                        #print(result_node)
+                    except TypeError as e:
+                        result_temp = result_temp # Obviously...
+                        print("fail: Bad call to", function.__name__, ":", e)
+            
+            result_node.append({'display_name': pipe.get('display_name', name),
+                                'type': type(result_temp).__name__,
+                                'value': result_temp})
+            
+        temp_state.results[node_id] = result[node_id] = {
+            'last_seen': node_data['last_seen'],
+            'outputs': result_node
         }
-    return results
 
+        # outfile_name = pipe.get('output_file', 'unknown')
+        # if '.json' not in outfile_name:
+        #     outfile_name = f"{outfile_name}.json"
+        # outdir_path = os.path.join(os.path.abspath(''), 'src', 'results', node_data.get('node_id', 'unknown'))
+        # if not os.path.exists(outdir_path):
+        #     os.makedirs(outdir_path)
 
-import os
+        # outfile_path = os.path.join(outdir_path, outfile_name)
 
-res = run_server_pipelines({'node_id': '2', 'value': [1, 23, 4]}, os.path.join(os.path.abspath('.'), 'pipelines', 'pipeline_server.yaml'))
-print(res)
+        # with open(outfile_path, 'w', encoding='utf-8') as f:
+        #     json.dump(result[name], f, ensure_ascii=False, indent=4)
+
+    return result
