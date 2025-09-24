ff7cb774534092c979367634576f1cd287d91711
afkisch <afkisch@gmail.com>
2025-09-24 23:24:29 +0200

    Move pipeline execution to /data API

--- src/api/ingest_api.py
+++ src/api/ingest_api.py
@@ -1,13 +1,18 @@
-import sys
-from pathlib import Path
+# TODO: add support for different bit sizes (16, 32, 64, ...)
+
 import numpy as np
 from datetime import datetime, timezone
 from utils.auth import require_api_key
-
-from flask import Blueprint, request, jsonify
-ingest_api = Blueprint('ingest_api', __name__)
+import pipeline_runner_server
+import os
+from db import db
+import json
 
 import temp_state
+
+from flask import Blueprint, request, jsonify, current_app
+ingest_api = Blueprint('ingest_api', __name__)
+
 
 @ingest_api.route('/api/v1/nodes/<node_id>/data', methods=['POST'])
 @require_api_key('node')
@@ -16,8 +21,8 @@ def receive_data(node_id):
     data = request.get_json()
     if not data:
         return jsonify({'error': 'Invalid or missing JSON data'}), 400
-    
-    value_received = np.array(data['value'])
+
+    value_received = np.array(data['value'], dtype=np.float32)                    # int32 for node
     if value_received.nbytes == data['size']:
         entry = {
             "node_id": node_id,
@@ -25,20 +30,40 @@ def receive_data(node_id):
             "value": data['value'],
             "last_seen": datetime.now(timezone.utc).isoformat()
         }
-        temp_state.latest_data[node_id] = entry
+
+        if node_id not in temp_state.nodes:
+            temp_state.nodes.append(node_id)                                            # TODO: Add TTL
+
+        current_app.cache.set(f'{node_id}:latest', entry)
+
+        #temp_state.latest_data[node_id] = entry
+        #temp_state.results = pipeline_runner_server.run_server_pipelines(entry, os.path.join(           # TODO: Move processing to dedicated thread with staging data
+        #    os.path.abspath('.'), 'pipelines', 'pipeline_server.yaml'))
+        
+        result = pipeline_runner_server.run_server_pipelines(entry, os.path.join(
+            os.path.abspath('.'), 'pipelines', 'pipeline_server.yaml'))
+        
+        current_app.cache.set(f'{node_id}:result', result)
+        
+        DB_PATH = os.path.join(os.path.abspath(''), 'src', 'db', 'node.db')                            # TODO: Move DB-operations to dedicated thread
+        db.log_to_db(node_id, data['value'], DB_PATH)
+        
         return jsonify({'status': 'success'}), 200
     else:
-        return jsonify({'error': f'Missing {abs(value_received.nbytes-data['size'])} bytes of JSON data'}), 400
-    
+        if value_received.nbytes < data['size']:
+            return jsonify({'error': f'Missing: {abs(value_received.nbytes-data['size'])} bytes of JSON data'}), 400
+        else:
+            return jsonify({'error': f'Overload: {abs(value_received.nbytes-data['size'])} bytes of JSON data'}), 400
 
-@ingest_api.route('/api/v1/nodes/<node_id>/heartbeat', methods=['POST'])
-#@require_api_key('node')
-def receive_heartbeat(node_id):
 
-    data = request.get_json()
-    
-    if not data:
-        return jsonify({'error': 'Invalid or missing JSON data'}), 400
-    
-    temp_state.latest_data[node_id]['last_seen'] = data.get('last_seen')
-    return jsonify({'status': 'success'}), 200
+# @ingest_api.route('/api/v1/nodes/<node_id>/heartbeat', methods=['POST'])
+# # @require_api_key('node')
+# def receive_heartbeat(node_id):
+
+#     data = request.get_json()
+
+#     if not data:
+#         return jsonify({'error': 'Invalid or missing JSON data'}), 400
+
+#     temp_state.latest_data[node_id]['last_seen'] = data.get('last_seen')
+#     return jsonify({'status': 'success'}), 200
