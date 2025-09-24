76df00f090dfcce092a0a92847cfdede81bf425a
afkisch <afkisch@gmail.com>
2025-09-24 23:12:51 +0200

    Add dictionary-based and redis cache

--- src/app.py
+++ src/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template
-from api.communication_api import comm_api
-from api.data_api import data_api
 from api.ingest_api import ingest_api
 from api.query_api import query_api
 
+from cache import DictCache, RedisCache
+
+from db import init
+import os
+db_path = os.path.join(os.path.abspath(''), 'src', 'db', 'node.db')
+init.create_node_table(db_path)
+
+
 app = Flask(__name__, template_folder='templates')
-app.register_blueprint(comm_api)
-app.register_blueprint(data_api)
+
+#app.cache = RedisCache()
+app.cache = DictCache()
+
 app.register_blueprint(ingest_api)
 app.register_blueprint(query_api)
-app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024  # 32 MB, for example
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024  # 64 MB, for example
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/config")
+def config():
+    return render_template("config.html")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
